9776dd18bb40f0b9c748484abd45e95dfe5916a8
Paul Morris-Hill <paul.morris-hill@parenta.com>
2022-01-11 20:16:52 +0000

    Removed old stuff

--- sql-scientist-ui/src/Settings.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-let Settings: React.FC = () => {
-    return <div></div>;
-};
-
-export default Settings;
-
--- sql-scientist-ui/src/test-run-settings/TestRunSettings.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-it('can render empty settings state', () => {
-    const div = document.createElement('div');
-    //ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
